c349b414142634e2ab312fd2de87e5aa6b387895
Mark Stone <markwstone85@gmail.com>
2019-10-13 02:21:23 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,7 +22,7 @@ class Doctor
   end
 
   def patients
-    Appointment.all.select {|appointment| appointment == appointment.self}
+    Appointment.all.select {|appointment| appointment.self.patient}
   end
 
 end
